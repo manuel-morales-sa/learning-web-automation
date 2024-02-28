45add7241880ad0aa00f5da2f0fafc3e0922594f
AldanaFasano <aldana.fasano@crehana.com>
2024-02-28 13:11:01 -0300

    Commmit Nuevo Proyecto parte3

--- src/test/java/testScript/LearningPath1Test.java
+++ src/test/java/testScript/LearningPath1Test.java
@@ -13,5 +13,57 @@ import java.util.concurrent.TimeUnit;
 
 public class LearningPath1Test extends BaseClass {
 
+    @Test
+    public void AAdminAddAndDeleteCourses() throws Exception {
+        LoginFunctions loginFunctions = new LoginFunctions(driver);
+        PanelPage panelPage = new PanelPage(driver);
+        LearningPathABMPage learningPathABMPage = new LearningPathABMPage(driver);
+        LearningPathPage learningPathPage = new LearningPathPage(driver);
 
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        loginFunctions.loginActions("manuel.automation@mailinator.com", "1234567890");
+        Thread.sleep(2000);
+
+        driver.navigate().to("https://www.crehana.com/org/qa-automation/panel/");
+        Thread.sleep(8000);
+        panelPage.buttonContent();
+        panelPage.buttonLearningPaths();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        learningPathABMPage.inputLearningPathsC();
+        WebElement elemento0 = learningPathABMPage.tittleLearningPaths();
+        Assert.assertEquals(elemento0.getText(), "Rutas de aprendizajes");
+        learningPathABMPage.inputLearningPaths("QA Agrega y Quita Curso");
+        Thread.sleep(6000);
+        learningPathABMPage.nameCourseQATrackConCursosRequeridos();
+        WebElement elemento1 = learningPathPage.titleCourseInCOurse();
+        Assert.assertEquals(elemento1.getText(), "QA Agrega y Quita Curso");
+        Thread.sleep(3000);
+        learningPathPage.addCourseIfNotExists();
+        Thread.sleep(3000);
+        learningPathPage.inputAddCoursesC();
+        learningPathPage.inputAddCourses("ia");
+        Thread.sleep(3000);
+        learningPathPage.addCourseButton();
+        learningPathPage.saveChangesAddCourses();
+        Thread.sleep(3000);
+        WebElement elemento2 = learningPathPage.popupAddCourseOK();
+        Assert.assertEquals(elemento2.getText(), "Curso(s) agregado(s) satisfactoriamente");
+        Thread.sleep(3000);
+        learningPathPage.deleteCourseCrehana();
+        learningPathPage.buttonDeleteCourse();
+        WebElement elemento3 = learningPathPage.popupQuestionDeleteMessage();
+        Assert.assertEquals(elemento3.getText(), "¿Estás seguro de que deseas eliminar el curso");
+        Thread.sleep(3000);
+        Thread.sleep(3000);
+        learningPathPage.buttonDeleteConfirm();
+        Thread.sleep(3000);
+        WebElement elemento5 = learningPathPage.popupAddCourseOK();
+        Assert.assertEquals(elemento5.getText(), "Curso de Notion IA borrado satisfactoriamente");
+        Thread.sleep(3000);
+        Thread.sleep(3000);
+        WebElement elemento6 = learningPathPage.validationCoursesTotal();
+        Assert.assertEquals(elemento6.getText(), "0\n" + "Total de cursos");
+        driver.navigate().refresh();
+    }
 }
